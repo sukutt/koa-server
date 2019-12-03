b2c223d152757478cf31afbc4f5e151baad4b019
woongahkim <realsukutt@gmail.com>
2019-12-04 01:35:18 +0900

    Complete mongodb run test

--- src/api/books/index.js
+++ src/api/books/index.js
@@ -5,8 +5,9 @@ const booksCtrl = require('./books.controller');
 
 books.get('/', booksCtrl.list);
 books.post('/', booksCtrl.create);
-books.delete('/', booksCtrl.delete);
-books.put('/', booksCtrl.replace);
-books.patch('/', booksCtrl.update);
+books.get('/:id', booksCtrl.get);
+books.delete('/:id', booksCtrl.delete);
+books.put('/:id', booksCtrl.replace);
+books.patch('/:id', booksCtrl.update);
 
 module.exports = books;
